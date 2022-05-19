fd87493771adf22b4663fda2cdba57f51cd63345
adachoinw <yuko.mizuki@gmail.com>
2022-05-18 21:30:41 -0400

    push to git

--- pong_game/main.py
+++ pong_game/main.py
@@ -28,6 +28,13 @@ while game_is_on:
 
     # Detect collision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+
+    # Detect collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 340:
+        ball.bounce_x()
+
+    if ball.distance(l_paddle) < 50 and ball.xcor() < -340:
+        ball.bounce_x()
 
 screen.exitonclick()
