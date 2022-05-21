3d00d1a1670f0d055687db7204cfff00c99116a0
Ada <yuko.mizuki@gmail.com>
2022-05-21 13:01:43 -0400

    05-21-2022

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -7,6 +7,7 @@ class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
+        self.high_score = 0
         self.score = 0
         self.penup()
         self.color("white")
@@ -15,15 +16,17 @@ class Scoreboard(Turtle):
         self.hideturtle()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGN, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGN, font=FONT)
 
-    def game_over(self):
-         self.goto(0, 0)
-         self.write("GAME OVER", align=ALIGN, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
 
 
     def inc_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
