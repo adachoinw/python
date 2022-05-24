73c4195b1320964a0238dd9e2862120736200225
adachoinw <yuko.mizuki@gmail.com>
2022-05-24 19:56:56 -0400

    init

--- day-25/main.py
+++ day-25/main.py
@@ -11,7 +11,7 @@
 
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
+# data = pandas.read_csv("weather_data.csv")
 # temp_list = data["temp"].to_list()
 # num_of_temp = len(temp_list)
 # avg_temp = sum(temp_list) / num_of_temp
@@ -20,18 +20,35 @@ data = pandas.read_csv("weather_data.csv")
 # print(data[data.temp == data.temp.max()])
 
 # Get data in row
-monday = data[data.day == "Monday"]
-cel = int(monday.temp)
-fah = (cel * 1.8) + 32
-print(fah)
+# monday = data[data.day == "Monday"]
+# cel = int(monday.temp)
+# fah = (cel * 1.8) + 32
+# print(fah)
+#
+#
+# # Create a dataframe from scratch
+# data_dict = {
+#     "students" : ["Amy", "James", "Angela"],
+#     "scores" : [76, 56, 65]
+# }
+#
+# data_1 = pandas.DataFrame(data_dict)
+# data_1.to_csv("new_student.csv")
 
 
-# Create a dataframe from scratch
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+gray_fur_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_fur_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_fur_count = len(data[data["Primary Fur Color"] == "Black"])
+print(gray_fur_count)
+print(red_fur_count)
+print(black_fur_count)
+
 data_dict = {
-    "students" : ["Amy", "James", "Angela"],
-    "scores" : [76, 56, 65]
+    "Fur color" : ["Gray", "Cinnamon", "Black"],
+    "Count" : [gray_fur_count, red_fur_count, black_fur_count]
 }
 
-data_1 = pandas.DataFrame(data_dict)
-data_1.to_csv("new_student.csv")
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_color.csv")
 
