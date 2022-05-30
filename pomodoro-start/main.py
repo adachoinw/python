da908a8f453a8866e32da7cbe5e367c83a5935cc
adachoinw <yuko.mizuki@gmail.com>
2022-05-30 11:24:27 -0400

    timer

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,16 +9,26 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
-
+def start_timer():
+    count_down(5 * 60)
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
-def start_func():
-    pass
+def start_timer():
+    global reps
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -26,17 +36,18 @@ window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=PINK)
 
 
+
 canvas = Canvas(width=200, height=224, bg=PINK, highlightthickness=0)
 image = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=image)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=2, row=2)
 
 
 timer_label = Label(text="Timer", bg=PINK, fg=GREEN, font=(FONT_NAME, 40, "bold"))
 timer_label.grid(column=2, row=1)
 
-start_button = Button(text="Start", highlightthickness=0)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=1, row=3)
 
 reset_button = Button(text="Reset", highlightthickness=0)
