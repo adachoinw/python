d03fb7f1f9f116e2b9a0143ad5c58e965977d26f
adachoinw <yuko.mizuki@gmail.com>
2022-06-13 19:26:59 -0400

    commit

--- day33-iss-api/main.py
+++ day33-iss-api/main.py
@@ -1,11 +1,35 @@
 import requests
+from datetime import datetime
 
-response = requests.get("http://api.open-notify.org/iss-now.json")
+MY_LAT = 43.653225
+MY_LNG = -79.383186
+
+#ISS
+# response = requests.get("http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+#
+# iss_position = (longitude, latitude)
+# print(iss_position)
+
+#Sunrise/sunset - required parameters for the API
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LNG,
+    "formatted": 0
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 
 data = response.json()
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+time_now = datetime.now()
 
-iss_position = (longitude, latitude)
-print(iss_position)
+print(sunrise)
+print(sunset)
+print(time_now.hour)
