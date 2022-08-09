a2ad50b740410bd93782f3e1f1f2f64d28d78e99
adachoinw <yuko.mizuki@gmail.com>
2022-08-09 17:54:07 -0400

    commit

---
+++ Day48-selenium-python-events/main.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+service = Service("Y:\Python Projects\development\chrome_driver\chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://www.python.org/")
+driver.maximize_window()
+
+datetime = driver.find_elements(By.CSS_SELECTOR, ".event-widget time")
+event_names = driver.find_elements(By.CSS_SELECTOR, ".event-widget li a")
+events = {}
+
+for n in range(len(datetime)):
+    events[n] = {
+        "time": datetime[n].text,
+        "name": event_names[n].text
+    }
+
+print(events)
+driver.quit()
