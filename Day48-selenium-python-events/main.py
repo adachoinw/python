05bd64bdf96ca42c5fc28c8168cd903296736214
adachoinw <yuko.mizuki@gmail.com>
2022-08-09 17:55:10 -0400

    commit

--- Day48-selenium-python-events/main.py
+++ Day48-selenium-python-events/main.py
@@ -6,7 +6,7 @@ service = Service("Y:\Python Projects\development\chrome_driver\chromedriver.exe
 driver = webdriver.Chrome(service=service)
 
 driver.get("https://www.python.org/")
-driver.maximize_window()
+driver.maximize_window() #maximizes window when open, so it can scrape the year
 
 datetime = driver.find_elements(By.CSS_SELECTOR, ".event-widget time")
 event_names = driver.find_elements(By.CSS_SELECTOR, ".event-widget li a")
